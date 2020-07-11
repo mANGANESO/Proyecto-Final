d32a8e3db5da43b686912f7344a04668239977f7
MARCOASG115 <marco.santoyo193@tectijuana.edu.mx>
2020-07-10 22:30:31 -0700

    Ya quedo la interfaz grafica del panel correspondiente a la funcion programacion. Pero falta hacer que los botones y las combobox hagan algo al seleccionarse o precionarse.

--- Reloj/Programar.java
+++ Reloj/Programar.java
@@ -4,69 +4,94 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.*;
 import java.util.Arrays;
-class Programar extends JFrame implements Runnable{
-	
-   JPanel panel2;
-   JButton btn;
-   JButton btn2;
-   JButton btn3;
-   JMenuItem item;
-   JComboBox tiempo;
-   public Programar(JMenuItem item){
-        this.item = item;
-        panel2 = new JPanel();
-        panel2.setLayout(null);
-        btn = new JButton("Guardar");
-        btn.setBounds(80,150,100,30);
-        btn2 = new JButton("");
-        btn2.setBounds(190,150,100,30);
-        btn3 = new JButton("Cancelar");
-        btn3.setBounds(300,150, 100,30);
+class Programar extends JFrame implements Runnable, ActionListener, ItemListener
+{
+  JPanel panel2;
+  JButton btn;
+  JButton btn2;
+  JButton btn3;
+  JMenuItem item;
+  JComboBox opHoras;
+  JComboBox opMinutos;
+  JComboBox opAmPm;
+  public Programar(JMenuItem item)
+  {
+    this.item = item;
+    panel2 = new JPanel();
+    panel2.setLayout(null);
+    panel2.setVisible(true);
+    btn = new JButton("Guardar");
+    btn.setBounds(80,150,100,30);
+    btn2 = new JButton("Cancelar");
+    btn2.setBounds(200,150,100,30);
+    btn3 = new JButton("Regresar");
+    btn3.setBounds(320,150, 100,30);
 
+    /*Datos de la comboBox de las Horas*/
+    int[] horas = new int[25];
 
+    for (int i =1 ;i <=24; i++ ) 
+    {	
+      horas[i] = i;	
+    }
 
-        /*Datos de la comboBox */
-         int[] timer = new int[1440];
+    String timerS[] = new String[horas.length]; 
+    for (int x = 1; x <horas.length; x++ ) 
+    {
+      timerS[x] = String.valueOf(horas[x]);       	
+    }
+    opHoras = new JComboBox(timerS);
+    opHoras.setBounds(80, 30, 100, 30);
+    opHoras.removeItemAt(0);
 
-         for (int i =1 ;i <=1440; i++ ) {
-        	
-        	timer[i] = i;
-        	
-        }
-        for (int i = 1;i<= 1440 ;i++ ) {
-        	System.out.println(timer[i]);
-        }
+    /*Datos de la comboBox de los Minutos*/
+    int[] minutos = new int[60];
+    for (int i =1 ;i <=59; i++ ) 
+    { 
+      minutos[i] = i; 
+    }
 
-        String timerS[] = new String[timer.length]; 
-        for (int x = 0; x <timer.length; x++ ) {
-        	timerS[x] = String.valueOf(timer[x]);
-        	System.out.println(Arrays.toString(timerS));
-        	
-        }
-        tiempo = new JComboBox(timerS);
-        tiempo.setBounds(250, 30, 100, 30);
+    String timerM[] = new String[minutos.length]; 
+    for (int y = 0; y <minutos.length; y++ ) 
+    {
+      timerM[y] = String.valueOf(minutos[y]);          
+    }
+    opMinutos = new JComboBox(timerM);
+    opMinutos.setBounds(200, 30, 100, 30);
+    /*Datos de la comboBox Am/Pm*/
+    String[] ampm = new String[2];
+    ampm[0]=("Am");
+    ampm[1]=("Pm");
+    opAmPm = new JComboBox(ampm);
+    opAmPm.setBounds(320, 30, 100, 30);
+    
+    panel2.add(opHoras);
+    panel2.add(opMinutos);
+    panel2.add(opAmPm);
+    panel2.add(btn);
+    panel2.add(btn2);
+    panel2.add(btn3);
 
+    this.add(panel2);
+    this.setSize(500,300);
+    this.setResizable(false);
+    this.setTitle("Programar...");
+    this.setDefaultCloseOperation(this.DISPOSE_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+  }
 
+  @Override
+  public void run()
+  {
 
+  }
+  public void actionPerformed(ActionEvent event)
+  {
+      
+  }
+  public void itemStateChanged(ItemEvent e)
+  {
 
-        panel2.add(tiempo);
-        panel2.add(btn);
-        panel2.add(btn2);
-        panel2.add(btn3);
-
-        this.add(panel2);
-        this.setSize(500,300);
-        this.setResizable(false);
-        this.setTitle("Programar...");
-        this.setDefaultCloseOperation(this.DISPOSE_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-   }
-
-   @Override
-   public void run(){
-
-
-
-   }
+  }
 }
--- Reloj/Reloj.java
+++ Reloj/Reloj.java
@@ -3,15 +3,17 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.*;
 import java.util.*;
- public class Reloj{
+public class Reloj
+{
   ///Reloj apk
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
   	ComponentesReloj reloj = new ComponentesReloj();
   }
- }
- class ComponentesReloj extends JFrame implements Runnable, ActionListener
- {
+}
 
+class ComponentesReloj extends JFrame implements Runnable, ActionListener
+{
   JLabel lblReloj;
   String hora,minuto,segundo,am_pm;
   Calendar Calendario;
@@ -22,68 +24,76 @@ import java.util.*;
   JMenu menu;
   JMenuItem item;
 
-  public ComponentesReloj(){
-  try{ 
-  /*panel inicializacion */
-  panel = new JPanel();
-  panel.setLayout(null);
-  panel.setFocusable(true);
-  panel.requestFocusInWindow();
-  /*Inicializacion etiqueta*/
-  lblReloj = new JLabel();
-  lblReloj.setBounds(0, 5, 500, 300);
-  lblReloj.setForeground(Color.GREEN);
-  lblReloj.setOpaque(true);
-  lblReloj.setBackground(Color.BLACK);
-  lblReloj.setFont(new Font("Calibri", 0, 80));
-  hilo1 = new Thread(this);
-  menuBar = new JMenuBar();
-  menu = new JMenu("Ajustes");
-  item = new JMenuItem("Programar");
+  public ComponentesReloj()
+  {
+    try
+    { 
+      /*panel inicializacion */
+      panel = new JPanel();
+      panel.setLayout(null);
+      panel.setFocusable(true);
+      panel.requestFocusInWindow();
+      /*Inicializacion etiqueta*/
+      lblReloj = new JLabel();
+      lblReloj.setBounds(0, 5, 500, 300);
+      lblReloj.setForeground(Color.GREEN);
+      lblReloj.setOpaque(true);
+      lblReloj.setBackground(Color.BLACK);
+      lblReloj.setFont(new Font("Calibri", 0, 80));
+      hilo1 = new Thread(this);
+      menuBar = new JMenuBar();
+      menu = new JMenu("Ajustes");
+      item = new JMenuItem("Programar");
    
-  menuBar.add(item);
-  menuBar.add(menu); 
-  panel.add(lblReloj);
+      menuBar.add(item);
+      menuBar.add(menu); 
+      panel.add(lblReloj);
 
-  /*Acciones del panel */
-  this.add(panel);
-  this.setSize(500 ,300);
-  this.setResizable(false);
-  this.setTitle("Reloj Emprendedor");
-  this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-  this.setLocationRelativeTo(null);
-  this.setVisible(true);
-  this.setJMenuBar(menuBar);
+      /*Acciones del panel */
+      this.add(panel);
+      this.setSize(500 ,300);
+      this.setResizable(false);
+      this.setTitle("Reloj Emprendedor");
+      this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+      this.setLocationRelativeTo(null);
+      this.setVisible(true);
+      this.setJMenuBar(menuBar);
 
-  item.addActionListener(this);
-  hilo1.start();
-
- }catch(Exception e){
-    System.out.println("Ha ocurrio un error al iniciar el programa :(");
-  }  
-}
-
-   @SuppressWarnings("unchecked")//Agregado para evitar los problemas tenidos a la hora de programar
-   @Override
-   public void run()
-   {
-    Thread ct = Thread.currentThread();
-
-    while(ct == hilo1){
-    calcula();
-    lblReloj.setText("  " + hora + ":" + minuto + ":" + segundo + " " + am_pm);
-
-    try{
-      Thread.sleep(1000);
-
-     }catch(Exception e){
-       System.out.println("Ha ocurrido un error al obtener la hora");
-     }
+      item.addActionListener(this);
+      hilo1.start();
     }
-   } 
 
-   //Obtiene la hora del ordenador
-   public void calcula(){
+    catch(Exception e)
+    {
+      System.out.println("Ha ocurrio un error al iniciar el programa :(");
+    }  
+  }
+
+  @SuppressWarnings("unchecked")//Agregado para evitar los problemas tenidos a la hora de programar
+  @Override
+  public void run()
+  {
+    Thread ct = Thread.currentThread();
+    while(ct == hilo1)
+    {
+      calcula();
+      lblReloj.setText("  " + hora + ":" + minuto + ":" + segundo + " " + am_pm);
+
+      try
+      {
+        Thread.sleep(1000);
+      }
+
+      catch(Exception e)
+      {
+        System.out.println("Ha ocurrido un error al obtener la hora");
+      }
+    }
+  }
+
+  //Obtiene la hora del ordenador
+  public void calcula()
+  {
     Calendar calendario = new GregorianCalendar();
     Date fecha = new Date();
     calendario.setTime(fecha); //obteniendo fecha
@@ -91,38 +101,45 @@ import java.util.*;
     am_pm = calendario.get(Calendar.AM_PM) == Calendar.AM?"AM":"PM";//determinando am o pm
 
     //condiciones si es AM
-    if (am_pm.equals("AM")) {
+    if (am_pm.equals("AM")) 
+    {
+      int horaDia = calendario.get(Calendar.HOUR_OF_DAY)-12;
+      hora = horaDia>9?"" + horaDia:"0" + horaDia;
+    }
 
-    int horaDia = calendario.get(Calendar.HOUR_OF_DAY)-12;
-
-    hora = horaDia>9?"" + horaDia:"0" + horaDia;
-
-    }else{
-
-    hora = calendario.get(Calendar.HOUR_OF_DAY) > 9? "" + calendario.get(Calendar.HOUR_OF_DAY): "0" + calendario.get(Calendar.HOUR_OF_DAY);
+    else
+    {
+      hora = calendario.get(Calendar.HOUR_OF_DAY) > 9? "" + calendario.get(Calendar.HOUR_OF_DAY): "0" + calendario.get(Calendar.HOUR_OF_DAY);
     }
 
     minuto = calendario.get(Calendar.MINUTE) > 9? "" + calendario.get(Calendar.MINUTE): "0" + calendario.get(Calendar.MINUTE);
     segundo = calendario.get(Calendar.SECOND) > 9? "" + calendario.get(Calendar.SECOND) : "0" + calendario.get(Calendar.SECOND);
+
     //Condiciones si es PM
-    if (am_pm.equals("PM")) {   
-    int horaDia = calendario.get(Calendar.HOUR_OF_DAY);
-    hora = horaDia>9?"" + horaDia:"0" + horaDia;
-    }else{
-    hora = calendario.get(Calendar.HOUR_OF_DAY) > 9? "" + calendario.get(Calendar.HOUR_OF_DAY): "0" + calendario.get(Calendar.HOUR_OF_DAY);
+    if (am_pm.equals("PM")) 
+    {   
+      int horaDia = calendario.get(Calendar.HOUR_OF_DAY);
+      hora = horaDia>9?"" + horaDia:"0" + horaDia;
+    }
+
+    else
+    {
+      hora = calendario.get(Calendar.HOUR_OF_DAY) > 9? "" + calendario.get(Calendar.HOUR_OF_DAY): "0" + calendario.get(Calendar.HOUR_OF_DAY);
     }
 
     minuto = calendario.get(Calendar.MINUTE) > 9? "" + calendario.get(Calendar.MINUTE): "0" + calendario.get(Calendar.MINUTE);
     segundo = calendario.get(Calendar.SECOND) > 9? "" + calendario.get(Calendar.SECOND) : "0" + calendario.get(Calendar.SECOND);
-   }
-   //oyente de los botones
-   public void actionPerformed(ActionEvent event){
-    //fijar hora de suspension
-    if (event.getSource() == item) {
+  }
 
+  //oyente de los botones
+  public void actionPerformed(ActionEvent event)
+  {
+    //fijar hora de suspension
+    if (event.getSource() == item) 
+    {
       Programar programar = new Programar(this.item);
       hilo2 = new Thread(programar);
       hilo2.start();
     }
-   }
- }
+  }
+}
